8b6bb981b298e70ea784f049adb3fae3589f91b6
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 11:21:11 +0100

    complet

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boucle1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop = 0;
+            while (loop < 10)
+            {
+                Console.WriteLine("Bonjour, je ne suis qu'un simple message.");
+                loop++;
+            }
+        }
+    }
+}
