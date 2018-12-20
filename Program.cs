5a4b82dac7b1e1ebb8afc5b59ec712e190789df1
MickaelHm <harmeau.mickael@gmail.com>
2018-12-20 10:38:13 +0100

    Correction

--- Program.cs
+++ Program.cs
@@ -11,11 +11,20 @@ namespace Boucle1
         static void Main(string[] args)
         {
             int loop = 0;
-            while (loop < 10)
+            while (loop < 10)//on continue la boucle tant qu'elle n'a pas atteint 10 révolutions.
             {
                 Console.WriteLine("Bonjour, je ne suis qu'un simple message.");
-                loop++;
+                loop++;//on incrémente la boucle à chaque révolution.
             }
+            /*Deuxieme méthode:
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Bonjour, je ne suis qu'un simple message.");
+                count++;//on incrémente la boucle à chaque révolution.
+            }
+            while (count < 10); 
+            ici l'instruction sera effectuée au moins une fois même si le count part de 10+*/
         }
     }
 }
